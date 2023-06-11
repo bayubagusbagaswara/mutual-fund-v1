519634eb54df18473d65aa0034ec4f5cff3a1ae5
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2023-06-11 21:31:34 +0700

    rename main application

--- src/main/java/com/bayu/mutualfundv1/MutualFundV1Application.java
+++ src/main/java/com/bayu/mutualfundv1/MutualFundV1Application.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.bayu.mutualfundv1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MutualFundV1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MutualFundV1Application.class, args);
 	}
 
 }
