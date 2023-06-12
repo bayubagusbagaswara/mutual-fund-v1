14cdc7b4bf66ad6bfb50eeda017e483a89e612dd
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2023-06-12 20:55:04 +0700

    add method findAllByModule

--- src/main/java/com/bayu/mutualfundv1/repository/FaqCategoryRepository.java
+++ src/main/java/com/bayu/mutualfundv1/repository/FaqCategoryRepository.java
@@ -4,11 +4,14 @@ import com.bayu.mutualfundv1.model.FaqCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface FaqCategoryRepository extends JpaRepository<FaqCategory, Long> {
 
     Optional<FaqCategory> findByCategoryCode(String categoryCode);
+
+    List<FaqCategory> findAllByModule(String module);
 
 }
