be765d88bd3763b6677113bec3964f593c7b75b1
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2023-06-12 20:51:23 +0700

    add method findAllByCategoryCode

--- src/main/java/com/bayu/mutualfundv1/repository/FaqRepository.java
+++ src/main/java/com/bayu/mutualfundv1/repository/FaqRepository.java
@@ -4,6 +4,11 @@ import com.bayu.mutualfundv1.model.Faq;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FaqRepository extends JpaRepository<Faq, Long> {
+
+    List<Faq> findAllByCategoryCode(String categoryCode);
+
 }
