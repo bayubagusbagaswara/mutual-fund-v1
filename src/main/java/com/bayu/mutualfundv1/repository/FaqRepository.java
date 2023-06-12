634381a05a81426646bd4d170b45ac3cda403d9e
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2023-06-12 20:52:10 +0700

    add method getAllFaqOrderByPopularLimit

--- src/main/java/com/bayu/mutualfundv1/repository/FaqRepository.java
+++ src/main/java/com/bayu/mutualfundv1/repository/FaqRepository.java
@@ -2,6 +2,8 @@ package com.bayu.mutualfundv1.repository;
 
 import com.bayu.mutualfundv1.model.Faq;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +13,6 @@ public interface FaqRepository extends JpaRepository<Faq, Long> {
 
     List<Faq> findAllByCategoryCode(String categoryCode);
 
+    @Query(value = "SELECT * FROM Faq f ORDER BY f.popular_order LIMIT = :limit", nativeQuery = true)
+    List<Faq> getAllFaqOrderByPopularLimit(@Param("limit") Integer limit);
 }
