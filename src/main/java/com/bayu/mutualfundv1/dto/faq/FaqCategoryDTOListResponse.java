c725bd875901b43b6fa15e69f3389f539e426ee4
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2023-06-13 09:00:51 +0700

    add dto FaqCategoryDTOListResponse

---
+++ src/main/java/com/bayu/mutualfundv1/dto/faq/FaqCategoryDTOListResponse.java
@@ -0,0 +1,15 @@
+package com.bayu.mutualfundv1.dto.faq;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FaqCategoryDTOListResponse {
+
+    private FaqCategoryDTOList faqCategoryDTOList;
+}
