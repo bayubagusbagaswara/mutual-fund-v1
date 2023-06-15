76f2efc5c511a84675c6e5158ba1ab254f50ebc3
Bayu Bagus Bagaswara <bayubagusbagaswara@gmail.com>
2023-06-15 10:08:14 +0700

    add service impl FaqServiceImpl

---
+++ src/main/java/com/bayu/mutualfundv1/service/impl/FaqServiceImpl.java
@@ -0,0 +1,21 @@
+package com.bayu.mutualfundv1.service.impl;
+
+import com.bayu.mutualfundv1.dto.faq.CreateFaqCategoryRequest;
+import com.bayu.mutualfundv1.dto.faq.FaqCategoryDTO;
+import com.bayu.mutualfundv1.repository.FaqCategoryRepository;
+import com.bayu.mutualfundv1.repository.FaqRepository;
+import com.bayu.mutualfundv1.service.FaqService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FaqServiceImpl implements FaqService {
+
+    private final FaqCategoryRepository faqCategoryRepository;
+    private final FaqRepository faqRepository;
+
+    public FaqServiceImpl(FaqCategoryRepository faqCategoryRepository, FaqRepository faqRepository) {
+        this.faqCategoryRepository = faqCategoryRepository;
+        this.faqRepository = faqRepository;
+    }
+
+}
