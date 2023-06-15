eea61d3437f2923a3b577d197d407880311d048f
Bayu Bagus Bagaswara <bayubagusbagaswara@gmail.com>
2023-06-15 10:07:04 +0700

    add method createFaqCategory

--- src/main/java/com/bayu/mutualfundv1/service/FaqService.java
+++ src/main/java/com/bayu/mutualfundv1/service/FaqService.java
@@ -1,4 +1,9 @@
 package com.bayu.mutualfundv1.service;
 
+import com.bayu.mutualfundv1.dto.faq.CreateFaqCategoryRequest;
+import com.bayu.mutualfundv1.dto.faq.FaqCategoryDTO;
+
 public interface FaqService {
+
+    FaqCategoryDTO createFaqCategory(CreateFaqCategoryRequest request);
 }
