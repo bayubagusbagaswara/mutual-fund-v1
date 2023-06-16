387114649332eed25bafb2efc4bfa33ffee8ee10
Bayu Bagus Bagaswara <bayubagusbagaswara@gmail.com>
2023-06-16 09:15:40 +0700

    add method contract getAllFaqCategoriesByModule

--- src/main/java/com/bayu/mutualfundv1/service/FaqService.java
+++ src/main/java/com/bayu/mutualfundv1/service/FaqService.java
@@ -3,7 +3,12 @@ package com.bayu.mutualfundv1.service;
 import com.bayu.mutualfundv1.dto.faq.CreateFaqCategoryRequest;
 import com.bayu.mutualfundv1.dto.faq.FaqCategoryDTO;
 
+import java.util.List;
+
 public interface FaqService {
 
     FaqCategoryDTO createFaqCategory(CreateFaqCategoryRequest request);
+
+    List<FaqCategoryDTO> getAllFaqCategoriesByModule(String module);
+
 }
