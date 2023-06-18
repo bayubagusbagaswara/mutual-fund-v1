39db47d4c1c1e98c27e8638ef0bd50472110ced2
Bayu Bagus Bagaswara <bayubagusbagaswara@gmail.com>
2023-06-18 21:09:57 +0700

    add handler getAllFaqCategory

--- src/main/java/com/bayu/mutualfundv1/controller/FaqMutualFundController.java
+++ src/main/java/com/bayu/mutualfundv1/controller/FaqMutualFundController.java
@@ -1,8 +1,16 @@
 package com.bayu.mutualfundv1.controller;
 
+import com.bayu.mutualfundv1.dto.faq.FaqCategoryDTO;
+import com.bayu.mutualfundv1.dto.faq.FaqCategoryDTOList;
+import com.bayu.mutualfundv1.dto.faq.FaqCategoryDTOListResponse;
 import com.bayu.mutualfundv1.service.FaqService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/mutual-fund/faq")
@@ -14,4 +22,15 @@ public class FaqMutualFundController {
     public FaqMutualFundController(FaqService faqCategoryService) {
         this.faqCategoryService = faqCategoryService;
     }
+
+    @GetMapping("/category/module/{module}")
+    public ResponseEntity<FaqCategoryDTOListResponse> getAllFaqCategory(@RequestParam(name = "module") String module) {
+        List<FaqCategoryDTO> faqCategorieList = faqCategoryService.getAllFaqCategoriesByModule(module);
+
+        FaqCategoryDTOListResponse response = FaqCategoryDTOListResponse.builder()
+                .faqCategoryDTOList(new FaqCategoryDTOList(faqCategorieList))
+                .build();
+        return ResponseEntity.ok(response);
+    }
+
 }
