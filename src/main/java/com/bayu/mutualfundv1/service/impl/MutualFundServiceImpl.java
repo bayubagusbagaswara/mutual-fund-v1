758ad4f093f1be2106a97f8837f93b34b2b798f7
Bayu Bagus Bagaswara <bayubagusbagaswara@gmail.com>
2023-06-18 21:06:10 +0700

    add method createMutualFund

--- src/main/java/com/bayu/mutualfundv1/service/impl/MutualFundServiceImpl.java
+++ src/main/java/com/bayu/mutualfundv1/service/impl/MutualFundServiceImpl.java
@@ -1,5 +1,7 @@
 package com.bayu.mutualfundv1.service.impl;
 
+import com.bayu.mutualfundv1.dto.mutualfund.CreateMutualFundRequest;
+import com.bayu.mutualfundv1.dto.mutualfund.CreateMutualFundResponse;
 import com.bayu.mutualfundv1.repository.MutualFundRepository;
 import com.bayu.mutualfundv1.service.MutualFundService;
 import org.springframework.stereotype.Service;
@@ -11,5 +13,10 @@ public class MutualFundServiceImpl implements MutualFundService {
 
     public MutualFundServiceImpl(MutualFundRepository mutualFundRepository) {
         this.mutualFundRepository = mutualFundRepository;
+    }
+
+    @Override
+    public CreateMutualFundResponse createMutualFund(CreateMutualFundRequest request) {
+        return null;
     }
 }
