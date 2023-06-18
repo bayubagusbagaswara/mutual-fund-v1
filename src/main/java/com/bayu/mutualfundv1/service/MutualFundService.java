890251c4105cab4852e3b381986bdea97ca494fc
Bayu Bagus Bagaswara <bayubagusbagaswara@gmail.com>
2023-06-18 21:05:41 +0700

    add method contract createMutualFund

--- src/main/java/com/bayu/mutualfundv1/service/MutualFundService.java
+++ src/main/java/com/bayu/mutualfundv1/service/MutualFundService.java
@@ -1,4 +1,20 @@
 package com.bayu.mutualfundv1.service;
 
+import com.bayu.mutualfundv1.dto.mutualfund.CreateMutualFundRequest;
+import com.bayu.mutualfundv1.dto.mutualfund.CreateMutualFundResponse;
+
 public interface MutualFundService {
+
+    // create mutual fund
+    CreateMutualFundResponse createMutualFund(CreateMutualFundRequest request);
+
+    // get mutual fund by id
+
+    // get mutual fund by code
+
+    // get all mutual fund
+
+    // update mutual fund
+
+    // delete mutual fund
 }
